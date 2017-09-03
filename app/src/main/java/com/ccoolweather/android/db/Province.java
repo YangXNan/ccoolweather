c3581ccee9bf9fd98c1a768c7166aefd69c25510
lenovo <lenovo>
2017-09-03 16:24:38 +0800

    Second commit.

---
+++ app/src/main/java/com/ccoolweather/android/db/Province.java
@@ -0,0 +1,40 @@
+package com.ccoolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by john on 17/9/2.
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private  int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
